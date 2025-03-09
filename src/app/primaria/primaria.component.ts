96bf597e9c8ddfd22dd4f1248e0020bcdbb90810
AdrianJavier7 <aperezmiranda@safareyes.es>
2025-03-09 22:40:40 +0100

    primaria medio medio

--- src/app/primaria/primaria.component.ts
+++ src/app/primaria/primaria.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-primaria',
@@ -11,38 +12,105 @@ import { IonicModule } from '@ionic/angular';
 export class PrimariaComponent implements OnInit {
   @ViewChild('rosco', { static: true }) roscoContainer!: ElementRef;
   @ViewChild('question', { static: true }) questionContainer!: ElementRef;
+  @ViewChild('empieza', { static: true }) empieza!: ElementRef;
 
-  letters: string[] = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".split("");
-  questions: { [key: string]: string } = {};
-  answers: { [key: string]: { A: string, B: string, correct: string } } = {};
-  currentLetter: string = "";
+  letras: string[] = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".split("");
+  preguntas: { [key: string]: string } = {};
+  respuestas: { [key: string]: string } = {};
+  letraActual: string = "";
+  letrasRespondidas: Set<string> = new Set();
+  correctAnswers: number = 0;
+  isModalOpen: boolean = false;
+  currentOptions: string[] = [];  // Definir currentOptions aquí
 
-  constructor() {
-    this.letters.forEach(letter => {
-      this.questions[letter] = `Con la ${letter}: Pregunta ejemplo.`;
-      this.answers[letter] = { A: `Respuesta A ${letter}`, B: `Respuesta B ${letter}`, correct: 'A' };
-    });
+  constructor(private toastController: ToastController) {
+    this.preguntas = {
+      "A": "Lo que se consideraban San Ignacio y sus compañeros al servir a Cristo.",
+      "B": "Punto de partida para su viaje de estudios y maduración espiritual.",
+      "C": "Orden religiosa fundada por San Ignacio de Loyola.",
+      "D": "Acción clave en la espiritualidad ignaciana para tomar decisiones según la voluntad de Dios.",
+      "E": "Nombre de la obra espiritual escrita por San Ignacio para la meditación y oración.",
+      "F": "Virtud teologal fundamental en la vida y enseñanzas de San Ignacio.",
+      "G": "Don divino que San Ignacio veía como esencial en la relación con Dios.",
+      "H": "Lo que sufrió San Ignacio en la batalla de Pamplona, iniciando su proceso de conversión.",
+      "I": "Nombre del fundador de la Compañía de Jesús.",
+      "J": "Nombre de aquel a quien San Ignacio dedicó su vida y su orden religiosa.",
+      "K": "Ciudad de Japón donde los jesuitas, siguiendo la misión iniciada por San Ignacio, evangelizaron en el siglo XVI.",
+      "L": "Lugar de nacimiento de San Ignacio en España.",
+      "M": "Colina en París donde Ignacio y sus compañeros hicieron sus primeros votos.",
+      "N": "Apellido de Jerónimo, uno de los grandes difusores de la espiritualidad ignaciana.",
+      "O": "Práctica fundamental en la vida de San Ignacio y base de sus Ejercicios Espirituales.",
+      "P": "Ciudad donde Ignacio fue herido en batalla en 1521.",
+      "Q": "Apellido de Antonio, jesuita misionero que difundió la espiritualidad ignaciana en América.",
+      "R": "Ciudad donde San Ignacio estableció la sede de la Compañía de Jesús.",
+      "S": "Título otorgado a San Ignacio y a otros compañeros jesuitas tras su canonización.",
+      "T": "Lugar al que Ignacio quiso peregrinar tras su conversión, pero no pudo quedarse.",
+      "U": "Lugar donde Ignacio estudió en París antes de fundar la Compañía de Jesús.",
+      "V": "Promesas que Ignacio y sus compañeros hicieron en Montmartre en 1534.",
+      "W": "Estado de EE.UU. donde los jesuitas llevaron su misión en el siglo XVII.",
+      "X": "Palabra en latín que significa paz, un valor central en la vida cristiana e ignaciana.",
+      "Y": "Apellido de un importante jesuita misionero en América.",
+      "Z": "Ciudad española donde Ignacio pasó en su camino antes de sus estudios en París."
+    };
+
+    this.respuestas = {
+      "A": "Apóstol",
+      "B": "Barcelona",
+      "C": "Compañía",
+      "D": "Discernir",
+      "E": "Ejercicios",
+      "F": "Fe",
+      "G": "Gracia",
+      "H": "Herida",
+      "I": "Ignacio",
+      "J": "Jesús",
+      "K": "Kioto",
+      "L": "Loyola",
+      "M": "Montmartre",
+      "N": "Nadal",
+      "O": "Oración",
+      "P": "Pamplona",
+      "Q": "Quirós",
+      "R": "Roma",
+      "S": "Santos",
+      "T": "Tierra Santa",
+      "U": "Universidad",
+      "V": "Votos",
+      "W": "Wisconsin",
+      "X": "Pax",
+      "Y": "Oyarzábal",
+      "Z": "Zaragoza"
+    };
   }
 
   ngOnInit() {
-    this.generateRosco();
+    this.generarRosco();
   }
 
-  generateRosco() {
-    const radius = 220;
-    const centerX = 175;
-    const centerY = 175;
-    const angleStep = (2 * Math.PI) / this.letters.length;
+  mostrarModalPuntuacion() {
+    this.isModalOpen = true;  // Abrir el modal
+  }
 
-    this.letters.forEach((letter, index) => {
-      const angle = index * angleStep - Math.PI / 2;
-      const x = centerX + radius * Math.cos(angle);
-      const y = centerY + radius * Math.sin(angle);
+  closeModal() {
+    this.isModalOpen = false;  // Cerrar el modal
+  }
 
-      const letterElement = document.createElement("div");
-      letterElement.textContent = letter;
+  generarRosco() {
+    const radio = 220;
+    const centroX = 175;
+    const centroY = 175;
+    const pasoDeAngulo = (2 * Math.PI) / this.letras.length;
 
-      Object.assign(letterElement.style, {
+    this.letras.forEach((letra, index) => {
+      const angulo = index * pasoDeAngulo - Math.PI / 2;
+      const x = centroX + radio * Math.cos(angulo);
+      const y = centroY + radio * Math.sin(angulo);
+
+      const elementoLetra = document.createElement("div");
+      elementoLetra.textContent = letra;
+
+      // Aplicar los estilos directamente
+      Object.assign(elementoLetra.style, {
         position: "absolute",
         width: "42px",
         height: "42px",
@@ -60,38 +128,99 @@ export class PrimariaComponent implements OnInit {
         transform: "translate(-50%, -50%)",
       });
 
-      letterElement.onclick = () => this.showQuestion(letter);
+      // Deshabilitar clic en letras ya respondidas
+      if (this.letrasRespondidas.has(letra)) {
+        elementoLetra.style.pointerEvents = 'none';
+      }
 
-      this.roscoContainer.nativeElement.appendChild(letterElement);
+      // Agregar evento de clic
+      elementoLetra.onclick = () => {
+        if (!this.letrasRespondidas.has(letra)) {
+          this.mostrarPregunta(letra);
+        }
+      };
+
+      // Agregar la letra al rosco
+      this.roscoContainer.nativeElement.appendChild(elementoLetra);
     });
   }
 
-  showQuestion(letter: string) {
-    this.currentLetter = letter;
-    this.questionContainer.nativeElement.textContent = this.questions[letter] || "Pregunta no disponible.";
+  mostrarPregunta(letra: string) {
+    this.letraActual = letra;
+    this.questionContainer.nativeElement.textContent = this.preguntas[letra] || "Pregunta no disponible.";
 
-    const answerAButton = document.querySelector('.boton1') as HTMLElement;
-    const answerBButton = document.querySelector('.boton2') as HTMLElement;
+    // Aquí se configuran las opciones de respuesta dinámicas
+    const opcionesIncorrectas = this.obtenerRespuestasIncorrectas(letra);
+    this.currentOptions = [this.respuestas[letra], ...opcionesIncorrectas];
+    this.currentOptions = this.shuffleArray(this.currentOptions); // Mezclar opciones
+  }
 
-    if (answerAButton && answerBButton) {
-      if (letter) {
-        answerAButton.style.display = 'block';
-        answerBButton.style.display = 'block';
-        answerAButton.textContent = this.answers[letter]?.A || "Respuesta A";
-        answerBButton.textContent = this.answers[letter]?.B || "Respuesta B";
-      } else {
-        answerAButton.style.display = 'none';
-        answerBButton.style.display = 'none';
+  obtenerRespuestasIncorrectas(letra: string): string[] {
+    const respuestasIncorrectas = Object.values(this.respuestas).filter((respuesta) => respuesta !== this.respuestas[letra]);
+    const respuestasAleatorias = [];
+
+    // Seleccionamos 3 respuestas incorrectas aleatorias
+    while (respuestasAleatorias.length < 3) {
+      const randomRespuesta = respuestasIncorrectas[Math.floor(Math.random() * respuestasIncorrectas.length)];
+      if (!respuestasAleatorias.includes(randomRespuesta)) {
+        respuestasAleatorias.push(randomRespuesta);
       }
+    }
+
+    return respuestasAleatorias;
+  }
+
+  shuffleArray(array: string[]): string[] {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  async checkAnswer(option: string) {
+    const correctAnswer = this.respuestas[this.letraActual];
+
+    if (option.toLowerCase() === correctAnswer.toLowerCase()) {
+      this.correctAnswers++;  // Incrementar el contador de respuestas correctas
+      await this.mostrarToast("¡Correcto!", 'success');
+      this.cambiarColorLetra(true);
+    } else {
+      await this.mostrarToast("Incorrecto, la respuesta era: " + correctAnswer, 'danger');
+      this.cambiarColorLetra(false);
+    }
+
+    // Marcar la letra como respondida
+    this.letrasRespondidas.add(this.letraActual);
+    this.actualizarRosco();
+
+    // Verificar si el juego ha terminado
+    if (this.letrasRespondidas.size === this.letras.length) {
+      this.mostrarModalPuntuacion();
     }
   }
 
-  checkAnswer(selectedAnswer: string) {
-    const correctAnswer = this.answers[this.currentLetter]?.correct;
-    if (correctAnswer && selectedAnswer === correctAnswer) {
-      alert("¡Correcto!");
-    } else {
-      alert("Incorrecto, la respuesta correcta era: " + (correctAnswer || "Desconocida"));
-    }
+  cambiarColorLetra(esCorrecto: boolean) {
+    const elementosLetras = this.roscoContainer.nativeElement.children;
+    Array.from(elementosLetras).forEach((elemento: any) => {
+      if (elemento.textContent === this.letraActual) {
+        elemento.style.backgroundColor = esCorrecto ? "green" : "red";
+      }
+    });
+  }
+
+  async mostrarToast(mensaje: string, color: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,  // Duración en milisegundos
+      color: color,    // El color del toast (puede ser 'success', 'danger', etc.)
+      position: 'bottom',  // Ubicación en la pantalla
+    });
+    toast.present();
+  }
+
+  actualizarRosco() {
+    const elementosLetras = this.roscoContainer.nativeElement.children;
+    Array.from(elementosLetras).forEach((elemento: any) => {
+      if (this.letrasRespondidas.has(elemento.textContent)) {
+        elemento.style.pointerEvents = 'none'; // Deshabilitar la letra si ya se respondió
+      }
+    });
   }
 }
